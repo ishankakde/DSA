c0e5c7cb2b87dc7115f913212d4ff1899c36453b
Ishan Kakde <ishan@Ishans-Mac-mini.local>
2025-09-07 14:07:04 -0500

    product of array

---
+++ 238. Product of Array Except Self/ProductOfArrayBruteForce.java
@@ -0,0 +1,40 @@
+/*
+
+leet code problem 238
+    - Product of Array Except Self
+    - https://leetcode.com/problems/product-of-array-except-self/
+
+Given an integer array nums, return an array answer such that answer[i] is equal to the product of all the elements of nums except nums[i].
+The product of any prefix or suffix of nums is guaranteed to fit in a 32-bit integer.
+You must write an algorithm that runs in O(n) time and without using the division operation.
+
+Example 1:
+
+Input: nums = [1,2,3,4]
+Output: [24,12,8,6]
+Example 2:
+
+Input: nums = [-1,1,0,-3,3]
+Output: [0,0,9,0,0]
+
+*/
+
+package com.dsa.lc;
+
+/*    - Time complexity - O(n2) - Brute force approach
+      - Space complexity - O(1)    */
+
+public class ProductOfArrayBruteForce {
+    public int[] productExceptSelf(int[] nums) {
+        int[] answer = new int[nums.length];
+        for(int i = 0; i < nums.length; i++){
+            answer[i] = 1;
+            for(int j = 0; j < nums.length; j++){
+                if(i != j){
+                    answer[i] = answer[i] * nums[j];
+                }
+            }
+        }
+        return answer;
+    }
+}
