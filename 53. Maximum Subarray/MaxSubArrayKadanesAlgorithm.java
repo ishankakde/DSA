4d4d792fdd5d50583667799d34a631ae8465d25b
Ishan Kakde <ishan@Ishans-Mac-mini.local>
2025-09-07 22:36:29 -0500

    using kadanes algorithm

---
+++ 53. Maximum Subarray/MaxSubArrayKadanesAlgorithm.java
@@ -0,0 +1,48 @@
+/*
+
+leet code problem 53
+    - level : medium
+    - Maximum Subarray
+    - https://leetcode.com/problems/maximum-subarray/
+
+Given an integer array nums, find the subarray (A subarray is a contiguous non-empty sequence
+of elements within an array) with the largest sum, and return its sum.
+
+Example 1:
+Input: nums = [-2,1,-3,4,-1,2,1,-5,4]
+Output: 6
+Explanation: The subarray [4,-1,2,1] has the largest sum 6.
+
+Example 2:
+Input: nums = [1]
+Output: 1
+Explanation: The subarray [1] has the largest sum 1.
+
+Example 3:
+Input: nums = [5,4,-1,7,8]
+Output: 23
+Explanation: The subarray [5,4,-1,7,8] has the largest sum 23.
+
+ */
+
+/*
+    Using Kadane's Algorithm
+ */
+package com.dsa.lc;
+
+public class MaxSubArrayKadanesAlgorithm {
+
+    public int maxSubArray(int[] nums) {
+        int maxSum = nums[0];
+        int currentSum = 0;
+
+        for(int i = 0; i < nums.length; i++){
+            currentSum = currentSum + nums[i];
+            maxSum = Math.max(maxSum, currentSum);
+            if(currentSum < 0){
+                currentSum = 0;
+            }
+        }
+        return maxSum;
+    }
+}
