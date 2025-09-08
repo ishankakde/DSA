55b6243658ae86afd809e5d23164e4b4dce5350c
Ishan Kakde <ishan@Ishans-Mac-mini.local>
2025-09-07 20:05:26 -0500

    contains duplicate

---
+++ 217. Contains Duplicate/ContainsDuplicate.java
@@ -0,0 +1,40 @@
+/*
+
+leet code problem 217
+    - Contains Duplicate
+    - https://leetcode.com/problems/contains-duplicate/
+
+Given an integer array nums, return true if any value appears at least twice in the array, and return false if every element is distinct.
+
+Example 1:
+Input: nums = [1,2,3,1]
+Output: true
+Explanation:
+The element 1 occurs at the indices 0 and 3.
+
+Example 2:
+Input: nums = [1,2,3,4]
+Output: false
+Explanation:
+All elements are distinct.
+
+Example 3:
+Input: nums = [1,1,1,3,3,4,3,2,4,2]
+Output: true
+
+ */
+package com.dsa.lc;
+
+import java.util.HashSet;
+
+public class ContainsDuplicate {
+    public boolean containsDuplicate(int[] nums) {
+        HashSet<Integer> set = new HashSet<>();
+        for(int i=0; i < nums.length; i++){
+            if(!set.add(nums[i])){
+                return true;
+            }
+        }
+        return false;
+    }
+}
