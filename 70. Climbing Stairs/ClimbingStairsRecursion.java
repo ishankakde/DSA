1e6a96b2324a835e618806d4550300c693694ac1
Ishan Kakde <ishan@Ishans-Mac-mini.local>
2025-09-15 07:36:45 -0500

    climbing stairs

---
+++ 70. Climbing Stairs/ClimbingStairsRecursion.java
@@ -0,0 +1,43 @@
+/*
+
+leet code problem 70
+    - level : easy
+    - Climbing Stairs
+    - https://leetcode.com/problems/climbing-stairs
+
+You are climbing a staircase. It takes n steps to reach the top.
+Each time you can either climb 1 or 2 steps. In how many distinct ways can you climb to the top?
+
+Example 1:
+Input: n = 2
+Output: 2
+Explanation: There are two ways to climb to the top.
+1. 1 step + 1 step
+2. 2 steps
+
+Example 2:
+Input: n = 3
+Output: 3
+Explanation: There are three ways to climb to the top.
+1. 1 step + 1 step + 1 step
+2. 1 step + 2 steps
+3. 2 steps + 1 step
+
+ */
+
+// this solution will result in TLE (Time Limit Exceeded) - Time complexity is O(2^n)
+package com.dsa.lc;
+
+public class ClimbingStairsRecursion {
+
+    public int climbStairs(int n) {
+
+        //base condition
+        if (n == 0 || n == 1) {
+            return 1;
+        }
+        //function logic
+        return climbStairs(n - 1) + climbStairs(n - 2);
+
+    }
+}
