09249d42cdf996979657f7c305c8c76e90103833
Ishan Kakde <ishan@Ishans-Mac-mini.local>
2025-09-15 08:20:08 -0500

    climbing staircase memoization

---
+++ 70. Climbing Stairs/ClimbingStairsMemoization.java
@@ -0,0 +1,48 @@
+/*
+
+leet code problem 70
+    - level : easy
+    - Climbing Stairs
+    - https://leetcode.com/problems/climbing-stairs
+
+You are climbing a staircase. It takes n steps to reach the top.
+Each time you can either climb 1 or 2 steps. In how many distinct ways can you climb to the top?
+
+Example 1:
+Input: n = 2
+Output: 2
+Explanation: There are two ways to climb to the top.
+1. 1 step + 1 step
+2. 2 steps
+
+Example 2:
+Input: n = 3
+Output: 3
+Explanation: There are three ways to climb to the top.
+1. 1 step + 1 step + 1 step
+2. 1 step + 2 steps
+3. 2 steps + 1 step
+
+ */
+
+/* Time complexity is O(n) */
+package com.dsa.lc;
+
+public class ClimbingStairsRecursion {
+
+    public int climbStairs(int n) {
+
+        //base condition
+        if (n == 0 || n == 1) {
+            return 1;
+        }
+
+        if(cacheMap.containsKey(n)){
+            return cacheMap.get(n);
+        }
+        //function logic
+        int result =  climbStairs(n - 1) + climbStairs(n - 2);
+        cacheMap.put(n, result);
+        return result;
+    }
+}
