720d9e14e2e301e76c55771fba6cc7f3d66a0634
Ishan Kakde <ishan@Ishans-Mac-mini.local>
2025-10-03 18:35:14 -0500

    3 sum

---
+++ 15. 3Sum/ThreeSumBruteForce.java
@@ -0,0 +1,64 @@
+/*
+leet code problem 15
+    - level : medium
+    - 3 Sum
+    - https://leetcode.com/problems/3sum
+
+Given an integer array nums, return all the triplets [nums[i], nums[j], nums[k]]
+such that i != j, i != k, and j != k, and nums[i] + nums[j] + nums[k] == 0.
+
+Notice that the solution set must not contain duplicate triplets.
+
+Example 1:
+Input: nums = [-1,0,1,2,-1,-4]
+Output: [[-1,-1,2],[-1,0,1]]
+
+Explanation:
+nums[0] + nums[1] + nums[2] = (-1) + 0 + 1 = 0.
+nums[1] + nums[2] + nums[4] = 0 + 1 + (-1) = 0.
+nums[0] + nums[3] + nums[4] = (-1) + 2 + (-1) = 0.
+The distinct triplets are [-1,0,1] and [-1,-1,2].
+Notice that the order of the output and the order of the triplets does not matter.
+
+Example 2:
+Input: nums = [0,1,1]
+Output: []
+Explanation: The only possible triplet does not sum up to 0.
+
+Example 3:
+Input: nums = [0,0,0]
+Output: [[0,0,0]]
+Explanation: The only possible triplet sums up to 0.
+
+
+Logic -
+
+use three for loops i = 0; i = j + 1, k = j + 1 to satisfy these conditions i != j, i != k, and j != k
+check if sum of any triplet is 0, if yes, then create a triplet list
+sort the triplet list to identify duplicates when adding in set.
+return unique triplets.
+
+ */
+package com.dsa.lc;
+
+import java.util.*;
+
+public class ThreeSumBruteForce {
+    public List<List<Integer>> threeSum(int[] nums) {
+
+        Set<List<Integer>> set = new HashSet<>();
+
+        for(int i = 0; i < nums.length; i++){
+            for(int j = i + 1; j < nums.length; j++){
+                for(int k = j + 1; k < nums.length; k++){
+                    if(nums[i] + nums[j] + nums[k] == 0){
+                        List<Integer> triplet = Arrays.asList(nums[i], nums[j], nums[k]);
+                        Collections.sort(triplet);
+                        set.add(triplet);
+                    }
+                }
+            }
+        }
+        return set.stream().toList();
+    }
+}
