6c35ea417e84e7e91b68b15fdc985d3b000b25e6
Ishan Kakde <ishan@Ishans-Mac-mini.local>
2025-10-03 12:46:04 -0500

    reverse an array

--- ReverseAnArray/ReverseAnArray.java
+++ ReverseAnArray/ReverseAnArray.java
@@ -1,5 +1,12 @@
 /*
 Reverse an Array
+
+logic -
+    - Two pointer approach
+    - swap elements at start and end index
+    - move start forward and end backwards
+    - while(start < end) works for both odd and even length arrays.
+    - for odd length the middle element does not get swapped as it remains at same position after reversal.
 */
 
 package com.dsa.lc;
@@ -8,7 +15,14 @@ public class ReverseAnArray {
 
     public static void main(String[] args) {
 
-        int[] nums = new int[]{1,2,3,4,5,6,7,8,9};
+        int[] even = new int[]{1,2,3,4,5,6,7,8};
+        int[] odd = new int[]{1,2,3,4,5,6,7,8,9};
+        reverse(even);
+        reverse(odd);
+
+    }
+
+    public static void reverse(int nums[]) {
 
         int start = 0;
         int end = nums.length - 1;
@@ -25,7 +39,6 @@ public class ReverseAnArray {
         for(int num : nums){
             System.out.print(num +" ");
         }
+        System.out.println();
     }
 }
-
-
