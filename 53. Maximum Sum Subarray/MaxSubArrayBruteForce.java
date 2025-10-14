0d8dcf474de009725dd953df193aa9d3e996f669
Ishan Kakde <ishan@Ishans-Mac-mini.local>
2025-10-13 21:19:46 -0500

    max sub array

--- 53. Maximum Sum Subarray/MaxSubArrayBruteForce.java
+++ 53. Maximum Sum Subarray/MaxSubArrayBruteForce.java
@@ -36,7 +36,7 @@ public class MaxSubArrayBruteForce {
 
     public int maxSubArray(int[] nums) {
 
-        int maxSum = nums[0];
+        int maxSum = Integer.MIN_VALUE;
         for(int i = 0; i < nums.length; i++){
             int currentSum = 0;
             for(int j = i; j < nums.length; j++){
