26e875edf4724b27c55c3a813a80be2338fad4f4
Ishan Kakde <ishan@Ishans-Mac-mini.local>
2025-10-17 15:14:52 -0500

    middle of LL

--- 876. Middle of the Linked List/MiddleNode.java
+++ 876. Middle of the Linked List/MiddleNode.java
@@ -8,8 +8,23 @@ leet code problem 876
 Given the head of a singly linked list, return the middle node of the linked list.
 If there are two middle nodes, return the second middle node.
 
+Example 1:
+Input: head = [1,2,3,4,5]
+Output: [3,4,5]
+Explanation: The middle node of the list is node 3.
+
+Example 2:
+Input: head = [1,2,3,4,5,6]
+Output: [4,5,6]
+Explanation: Since the list has two middle nodes with values 3 and 4, we return the second one.
+
 logic -
-when fast pointer reaches the end the slow pointer reaches the middle of the linked list.
+
+Initialize two pointers, slow and fast, at the head of the linked list.
+Move the slow pointer one node at a time and the fast pointer two nodes at a time.
+Continue traversing the list until the fast pointer reaches the end (i.e., becomes null or its next is null).
+At this point, the slow pointer will be positioned at the middle node of the list.
+Return the slow pointer as the middle node.
 
  */
 package com.dsa.lc;
