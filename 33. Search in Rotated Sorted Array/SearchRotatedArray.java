38f7bf020868999589ce32e8043f91dedb467394
Ishan Kakde <ishan@Ishans-Mac-mini.local>
2025-10-23 10:48:14 -0500

    rotated array

--- 33. Search in Rotated Sorted Array/SearchRotatedArray.java
+++ 33. Search in Rotated Sorted Array/SearchRotatedArray.java
@@ -41,27 +41,26 @@ public class SearchRotatedArray {
         int end = nums.length - 1;
 
         while (start <= end) {
-
             int mid = start + (end - start) / 2;
-            if (nums[mid] == target) {
+
+            if(target == nums[mid]) {
                 return mid;
             }
 
             if (nums[mid] > nums[end]) { // left sorted
-                if (target >= nums[start] && target <= nums[mid]) {
+                if (target >= nums[start] && target < nums[mid]) {
                     end = mid - 1;
                 } else {
                     start = mid + 1;
                 }
             } else { //right sorted
-                if (target >= nums[mid] && target <= nums[end]) {
+                if (target > nums[mid] && target <= nums[end]) {
                     start = mid + 1;
                 } else {
                     end = mid - 1;
                 }
             }
         }
-
         return -1;
     }
 }
